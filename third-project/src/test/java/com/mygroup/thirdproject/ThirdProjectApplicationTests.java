32528ab525295654ca06e189e6471fd154fa4f7e
hyeon <guseh5634@naver.com>
2023-07-31 15:11:51 +0900

    first commit

---
+++ third-project/src/test/java/com/mygroup/thirdproject/ThirdProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mygroup.thirdproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirdProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
